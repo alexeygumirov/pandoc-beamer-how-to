0b5b7f560ecd50069f6f9cbc9467ca07b04f869a
Alexey Gumirov <a.gumirov@telekom.com>
2020-05-28 10:36:55 +0200

    Quotes/notes are more visible now

--- _pres-gen.sh
+++ _pres-gen.sh
@@ -2,6 +2,7 @@
 
 DATE_COVER=$(date "+%d %B %Y")
 SOURCE_FORMAT="markdown_github+yaml_metadata_block+implicit_figures+all_symbols_escapable+link_attributes+smart"
-DATA_DIR="pandoc"
+# DATA_DIR="pandoc"
 
-pandoc -s --dpi=300 --slide-level 2 --toc --listings --shift-heading-level=0 --data-dir="${DATA_DIR}" --template default_mod.latex --pdf-engine xelatex -f "$SOURCE_FORMAT" -M date="$DATE_COVER" -V classoption:aspectratio=169 -V lang=en-US -t beamer presentation.md -o presentation.pdf
+# pandoc -s --dpi=300 --slide-level 2 --toc --listings --shift-heading-level=0 --data-dir="${DATA_DIR}" --template default_mod.latex --pdf-engine xelatex -f "$SOURCE_FORMAT" -M date="$DATE_COVER" -V classoption:aspectratio=169 -V lang=en-US -t beamer presentation.md -o presentation.pdf
+pandoc -s --dpi=300 --slide-level 2 --toc --listings --shift-heading-level=0 --template default_mod.latex --pdf-engine xelatex -f "$SOURCE_FORMAT" -M date="$DATE_COVER" -V classoption:aspectratio=169 -V lang=en-US -t beamer presentation.md -o presentation.pdf
